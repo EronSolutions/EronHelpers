5d11cd51ed0bdf4f72b4005fd4d50fa4e383b09f
Mohammad Rebati <m.rebati@icloud.com>
2018-09-07 23:54:52 +0430

    add unit test to .net codes.
    added some unit tests to the project.

---
+++ EronSolutions.Herlpers.CSharpNet.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EronSolutions.Herlpers.CSharpNet.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EronSolutions.Herlpers.CSharpNet.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("99974cb8-7e10-4ee6-8351-8760ae742aa7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EronSolutions.Herlpers.CSharpNet.Tests/StringHtmlExtensionTests.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EronSolutions.Herlpers.CSharpNet.Tests
+{
+    /// <summary>
+    /// Summary description for StringHtmlExtensionTests
+    /// </summary>
+    [TestClass]
+    public class StringHtmlExtensionTests
+    {
+        public StringHtmlExtensionTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void ToDelimitedStringTest()
+        {
+            Assert.AreEqual("", (new string[] { }).ToDelimitedString(","));
+            Assert.AreEqual("", ((string[])null).ToDelimitedString(","));
+            Assert.AreEqual("one", (new string[] { "one" }).ToDelimitedString(", "));
+            Assert.AreEqual("one, two", (new string[] { "one", "two" }).ToDelimitedString(", "));
+            Assert.AreEqual("one,two", (new string[] { "one", "two" }).ToDelimitedString(","));
+        }
+
+        [TestMethod]
+        public void StripHtmlTest()
+        {
+            Assert.IsNull(((string)null).StripHtml());
+
+            Assert.AreEqual("hello", "hello".StripHtml());
+
+            Assert.AreEqual("hello", "he<b>ll</b>o".StripHtml());
+        }
+
+        [TestMethod]
+        public void TruncateTextTest()
+        {
+            Assert.IsNull(((string)null).StripHtml());
+
+            string test = "1234567890";
+            Assert.AreEqual("12345", test.Truncate(5, null));
+            Assert.AreEqual("12345...", test.Truncate(5, "..."));
+            Assert.AreEqual(string.Empty, string.Empty.Truncate(5, null));
+            Assert.AreEqual("12", "12".Truncate(5));
+        }
+
+        [TestMethod]
+        public void TruncateHtmlEmptyTest()
+        {
+            Assert.IsNull(((string)null).TruncateHtml(100));
+            Assert.AreEqual(string.Empty.TruncateHtml(100), string.Empty);
+        }
+
+        [TestMethod]
+        public void TruncateHtmlTextTest()
+        {
+            // no html test
+            Assert.AreEqual("abc".TruncateHtml(10), "abc");
+            Assert.AreEqual("abc".TruncateHtml(2), "ab");
+        }
+
+        [TestMethod]
+        public void TruncateHtmlTextByDelimiterTest()
+        {
+            const string testHtml = "<b>hello</b><!-- ReadMore --><div>abc</div>";
+
+            // Test for a "read more" delimiter using ordinal comparisons
+            Assert.AreEqual(testHtml.TruncateHtmlByDelimiter("<!-- ReadMore -->"), "<b>hello</b>");
+            Assert.AreNotEqual(testHtml.TruncateHtmlByDelimiter("<!-- READMORE -->"), "<b>hello</b>");
+
+            // Test for a "read more" delimiter using ordinal ignore case comparisons
+            Assert.AreEqual(testHtml.TruncateHtmlByDelimiter("<!-- ReadMore -->", StringComparison.OrdinalIgnoreCase), "<b>hello</b>");
+            Assert.AreEqual(testHtml.TruncateHtmlByDelimiter("<!-- READMORE -->", StringComparison.OrdinalIgnoreCase), "<b>hello</b>");
+
+            // Test for a delimiter that does not exists using ordinal comparison
+            Assert.AreEqual(testHtml.TruncateHtmlByDelimiter("<!-- IDontExist -->"), testHtml);
+
+            // Test truncating with a delimiter that leaves an open tag
+            Assert.AreEqual(testHtml.TruncateHtmlByDelimiter("</b>"), "<b>hello</b>");
+        }
+
+
+        [TestMethod]
+        public void TruncateHtmlTest()
+        {
+            var html = @"<p>aaa <b>bbb</b>
+ccc<br> ddd</p>";
+
+            Assert.AreEqual(@"<p>aaa <b>bbb</b>
+ccc<br> ddd</p>", html.TruncateHtml(100, "no trailing text")); // it ignores unclosed tags
+
+            Assert.AreEqual(@"<p>aaa <b>bbb</b>
+ccc<br>...</br></p>", html.TruncateHtml(14, "..."));
+
+            Assert.AreEqual(@"<p>aaa <b>bbb</b></p>", html.TruncateHtml(10));
+
+            // self closing test
+            html = @"<p>hello<br/>there</p>";
+            Assert.AreEqual(@"<p>hello<br/>th</p>", html.TruncateHtml(7));
+
+            Assert.AreEqual("<b>i'm</b>", "<b>i'm awesome</b>".TruncateHtml(8));
+            Assert.AreEqual("<b>i'm...</b>", "<b>i'm awesome</b>".TruncateHtml(8, "..."));
+        }
+
+        [TestMethod]
+        public void TruncateWordsTest()
+        {
+            Assert.IsNull(((string)null).TruncateWords(100));
+            Assert.AreEqual(string.Empty, string.Empty.TruncateWords(100));
+
+            Assert.AreEqual("big brown", "big brown beaver".TruncateWords(12));
+            Assert.AreEqual("big...", "big brown beaver".TruncateWords(5, "..."));
+        }
+
+        [TestMethod]
+        public void TruncateWordsBreakingHtmlTagTest()
+        {
+            // truncates in the middle of a tag
+            Assert.AreEqual("<b>i'm", "<b>i'm awesome</b>".TruncateWords(16));
+        }
+    }
+}
